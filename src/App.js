f539152fc0a9e04aafdf4e5b879a9709aa73b5c4
adeckert1010 <adeckert1010@gmail.com>
2020-09-25 10:45:22 -0400

    Fresh Start

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ul>
+          <li>#1.</li>
+          <li>#2.</li>
+          <li>#3.</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
